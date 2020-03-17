0db6baaea155c33d487efaa793c718f7c8b02d80
TadiyosGirma <54555296+TadiyosGirma@users.noreply.github.com>
2020-03-17 14:55:36 -0500

    @CrossOrigin change in the controller
    @CrossOrigin change in the controller

--- src/main/java/miu/edu/studentenrollment/controller/FacultyController.java
+++ src/main/java/miu/edu/studentenrollment/controller/FacultyController.java
@@ -3,6 +3,7 @@ package miu.edu.studentenrollment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import miu.edu.studentenrollment.domain.Faculty;
 import miu.edu.studentenrollment.service.FaculityService;
 
 @RestController
+@CrossOrigin
 public class FacultyController {
 
 	@Autowired
---
+++ src/test/java/miu/edu/studentenrollment/serviceimptest/FacultyServiceImplTest.java
@@ -0,0 +1,84 @@
+package miu.edu.studentenrollment.serviceimptest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import miu.edu.studentenrollment.domain.Faculty;
+import miu.edu.studentenrollment.service.impl.FaculityServiceImpl;
+
+@DisplayName("Faculty Services Test Cases")
+public class FacultyServiceImplTest {
+
+//	@InjectMocks
+//	EntryController entryController;
+//
+//	MockMvc mockMvc;
+
+	@Mock
+	FaculityServiceImpl facultyService;
+
+	Faculty faculty1 = new Faculty();
+	Faculty faculty2 = new Faculty();
+	List<Faculty> facultyList = new ArrayList<>();
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+//		mockMvc = MockMvcBuilders.standaloneSetup(entryController).build();
+		faculty1.setId(1L);
+		faculty1.setFacultyName("Payman Salek");
+		faculty1.setFacultyTitle("professor");
+		facultyList.add(faculty1);
+		facultyList.add(faculty2);
+		// faculty1.setSection(section);
+	}
+
+	@Test
+	void testAddFaculity() {
+		try {
+			when(facultyService.addFaculity(faculty1)).thenReturn(faculty1);
+			assertEquals(faculty1, facultyService.addFaculity(faculty1));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	void testUpdateFaculity() {
+		try {
+			when(facultyService.updateFaculity(faculty2)).thenReturn(faculty2);
+			assertEquals(faculty2, facultyService.updateFaculity(faculty2));
+		} catch (Exception e) {
+
+		}
+	}
+
+	@Test
+	void testGetAllFaculity() {
+		try {
+			when(facultyService.getAllFaculity()).thenReturn(facultyList);
+			assertEquals(2, facultyService.getAllFaculity().size());
+		} catch (Exception e) {
+
+		}
+	}
+
+	@Test
+	void testGetOneFaculty() {
+		try {
+			when(facultyService.getOneFaculty(1L)).thenReturn(faculty1);
+			assertEquals(faculty1, facultyService.getOneFaculty(1L));
+		} catch (Exception e) {
+
+		}
+	}
+}
