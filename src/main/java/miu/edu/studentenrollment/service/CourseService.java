49e0e8294d8fd9209bac72e89b379441c8585fd1
thierry.manzi <thierry.manzi001@gmail.com>
2020-03-18 12:14:18 -0500

    course naming convention fixed

--- src/main/java/miu/edu/studentenrollment/service/CourseService.java
+++ src/main/java/miu/edu/studentenrollment/service/CourseService.java
@@ -7,7 +7,7 @@ import miu.edu.studentenrollment.domain.Course;
 
 public interface CourseService {
 	public Course createCourse(Course course);
-	public List<Course> courses();
+	public List<Course> getAllCourses();
 	public Course updateCourse(Course course) throws Exception;
 	public String removeCourse(Course course);
 	public Course getCourseById(Long id);
