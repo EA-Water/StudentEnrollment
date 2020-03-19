2891736fd827b373874a22fb1a3c9f40cf15043e
Meron <meronmohammed00@gmail.com>
2020-03-19 00:16:43 -0500

    offering security

--- src/main/java/miu/edu/studentenrollment/controller/OfferingController.java
+++ src/main/java/miu/edu/studentenrollment/controller/OfferingController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import miu.edu.studentenrollment.service.OfferingService;
 import miu.edu.studentenrollment.util.CustomError;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/offerings")
 public class OfferingController {
 	
--- src/main/java/miu/edu/studentenrollment/service/OfferingService.java
+++ src/main/java/miu/edu/studentenrollment/service/OfferingService.java
@@ -2,28 +2,38 @@ package miu.edu.studentenrollment.service;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import miu.edu.studentenrollment.domain.Offering;
 
 public interface OfferingService {
 
     Offering getOfferingById(long id);
-
+    
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     List<Offering> getAllOffering();
     
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     List<Offering> getOfferingsByBlockId(long id);
     
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     List<Offering> getOfferingsByBlockCodeOrCourseCode(String blockCode);
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     List<Offering> getOfferingsByCourseId(long id);
-
+    
+    @PreAuthorize("hasRole('ADMIN')")
     void createOffering(Offering offering);
     
     boolean isOfferingExit(Offering offering);
     
+    @PreAuthorize("hasRole('ADMIN')")
     void updateOffering(Offering offering);
     
+    @PreAuthorize("hasRole('ADMIN')")
     void removeOffering(long id);
     
+    @PreAuthorize("hasRole('ADMIN')")
     void removeAllOfferings();
 
 }
